859d9a22d9a4a7012182780660b62686d2000261
Tony Foster <47356452+wondernerd@users.noreply.github.com>
2026-09-25 12:05:12 -0500

    Update Horizon_API-List_DS_Pools.ps1
    Corrected dash type in the login token string and an extra space between the dash and method in the get pool call.

--- Horizon_API-List_DS_Pools.ps1
+++ Horizon_API-List_DS_Pools.ps1
@@ -9,7 +9,7 @@ $Body = @{
     "domain"   = "YOURDOMAIN"} | ConvertTo-Json
     
 # Request the login token
-$LoginResponse = Invoke-RestMethod –Uri "$BaseUrl/login" -Method Post -Body $Body -ContentType "application/json"
+$LoginResponse = Invoke-RestMethod -Uri "$BaseUrl/login" -Method Post -Body $Body -ContentType "application/json"
 
 # Capture the Bearer token (Horizon returns access_token)
 $BearerToken = $LoginResponse.access_token
@@ -25,7 +25,7 @@ $Headers = @{
 $DSpoolPath = "inventory/v1/desktop-pools"
 
 #API Get call
-$DSPoolList = Invoke-RestMethod -Uri "$BaseUrl/$DSpoolPath" -	Method Get -Headers $Headers
+$DSPoolList = Invoke-RestMethod -Uri "$BaseUrl/$DSpoolPath" -Method Get -Headers $Headers
 
 #Display the result
 $DSPoolList
