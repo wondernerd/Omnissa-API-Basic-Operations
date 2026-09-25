49fec2913e35286b85f4b3438b92309581212092
Tony Foster <47356452+wondernerd@users.noreply.github.com>
2026-09-25 12:13:10 -0500

    Update Horizon_API-Disable_DS_Pool.ps1
    Corrected several bad dashes and some tabs that were in the code.

--- Horizon_API-Disable_DS_Pool.ps1
+++ Horizon_API-Disable_DS_Pool.ps1
@@ -9,7 +9,7 @@ $Body = @{
     "domain"   = "YOURDOMAIN"} | ConvertTo-Json
     
 # Request the login token
-$LoginResponse = Invoke-RestMethod –Uri "$BaseUrl/login" -Method Post -Body $Body -ContentType "application/json"
+$LoginResponse = Invoke-RestMethod -Uri "$BaseUrl/login" -Method Post -Body $Body -ContentType "application/json"
 
 # Capture the Bearer token (Horizon returns access_token)
 $BearerToken = $LoginResponse.access_token
@@ -25,7 +25,7 @@ $Headers = @{
 $DSpoolPath = "inventory/v1/desktop-pools"
 
 #Get pool information
-$DSPoolList = Invoke-RestMethod -Uri "$BaseUrl/$DSPoolPath" -Method Get –Headers 	$Headers 
+$DSPoolList = Invoke-RestMethod -Uri "$BaseUrl/$DSPoolPath" -Method Get –Headers $Headers 
 
 #Create JSON list of desktop pools
 $DSpoolID = $DSPoolList.id
@@ -33,4 +33,4 @@ $DSPoolRawIDs = @("$DSpoolID")
 $DSPoolJSONIDs = ConvertTo-json -inputobject $DSPoolRawIDs #for PowerShell 5.1+, because this is only a single ID and the JSON format expects an array we need to use the inputobject method to put it in [brackets] 
 
 #Disable desktop pools
-$DSPoolDisResult = Invoke-RestMethod -Uri "$BaseUrl/$DSpoolPath/action/disable" -Method Post -Headers $Headers -Body $DSPoolJSONIDs -contenttype "application/json"
+$DSPoolDisResult = Invoke-RestMethod -Uri "$BaseUrl/$DSpoolPath/action/disable" -Method Post -Headers $Headers -Body $DSPoolJSONIDs -contenttype "application/json"
